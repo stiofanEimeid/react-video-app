f6c527ae6daffebd5d4880abda5587648548a47c
Stephen Byrne <s.byrne6@gmail.com>
2020-07-13 21:21:38 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # README
+
+A React application serving videos from the Youtube API that makes use of hooks. This site was built by following Stephen Grider's Udemy Course, [Modern React with Redux](https://www.udemy.com/course/react-redux/).
--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import useVideos from '../hooks/useVideos';
 
 const App = () => {
     const [selectedVideo, setSelectedVideo] = useState(null);
-    const [videos, search] = useVideos('buildings');
+    const [videos, search] = useVideos('penguins');
 
     useEffect(()=> {
         setSelectedVideo(videos[0])
